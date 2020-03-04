f66c6565d1c88738590d2d65d06ca0e19a12e026
Matt Knight <matthew.knight@gmail.com>
2020-03-04 14:03:43 -0700

    Support default values for config

--- src/index.js
+++ src/index.js
@@ -4,21 +4,44 @@ import path from 'path';
 const configs = {};
 const secrets = {};
 
+export const getConfig = key => configs[key] || null;
+
+export const getSecret = key => secrets[key] || null;
+
 export const initConfig = (keys = [], { log } = {}) => {
   keys.forEach(k => {
-    const key = k.toUpperCase();
+    let key;
+    let defaultValue = null;
 
-    configs[k] = process.env[key] || null;
+    if (typeof k === 'string') {
+      key = k.toUpperCase();
+    } else if (typeof k === 'object') {
+      ({ name: key, defaultValue = null } = k);
+    } else {
+      throw new Error(`Invalid key type found: ${k}`);
+    }
 
-    if (process.env.hasOwnProperty(key)) {
-      configs[k] = process.env[key];
+    if (!key) {
+      throw new Error('Config variable name not provided');
+    }
+
+    const envKey = key.toUpperCase();
+
+    configs[key] = process.env[envKey] || defaultValue;
+
+    if (process.env.hasOwnProperty(envKey)) {
+      configs[key] = process.env[envKey];
       log &&
         log.info(
-          { env: { name: key, config: k, value: configs[k] } },
+          { env: { name: key, config: k, value: configs[key] } },
           `Loaded configuration variable`
         );
     } else {
-      log && log.info({ env: { name: key, config: k } }, `Expected environment variable not set`);
+      log &&
+        log.info(
+          { env: { name: key, config: k, value: configs[key] } },
+          `Expected environment variable not set; using default`
+        );
     }
   });
 };
@@ -46,7 +69,3 @@ export const initSecrets = (keys = [], { log, dir, trim = true } = {}) => {
     }
   });
 };
-
-export const getConfig = key => configs[key] || null;
-
-export const getSecret = key => secrets[key] || null;
